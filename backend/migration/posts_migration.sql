c8d2ba7fc8dedb5eed0212be05d1c2480e300e71
alfarcats <kuroyuki2039@icloud.com>
2025-02-26 17:17:33 +0900

    create sql

---
+++ backend/migration/posts_migration.sql
@@ -0,0 +1,9 @@
+CREATE TABLE posts (
+    id INT AUTO_INCREMENT, 
+    user_id INT, post_id INT, 
+    picture TEXT, text TEXT,
+    assignment_point INT, 
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, 
+    PRIMARY KEY (id)
+     );
