6c721733338ce8515ee0bb610e45c58bc20b7c36
taka-0305 <tobita.hal@gmail.com>
2025-02-26 18:14:54 +0900

    feat:タイムラインと投稿のコンポーネントを作成

--- src/app/pages/Timeline/page.tsx
+++ src/app/pages/Timeline/page.tsx
@@ -1,8 +1,36 @@
 import Link from "next/link";
+import Post from "@/app/pages/Timeline/components/post/post";
 
 const TimeLine = () => (
-  <div className="mt-8 w-full flex flex-col gap-4 justify-center items-center">
-    TimeLineのページだよ
+  <div className="mt-8 w-full flex flex-col gap-8 justify-center items-center">
+    <Post 
+    user_id = "user_id.test"
+    post_id = {123456}
+    text="test"
+    like_count={8888}
+    comment_count = {8888}
+    />
+    <Post 
+    user_id = "user_id.test"
+    post_id = {123456}
+    text="test"
+    like_count={8888}
+    comment_count = {8888}
+    />
+    <Post 
+    user_id = "user_id.test"
+    post_id = {123456}
+    text="test"
+    like_count={8888}
+    comment_count = {8888}
+    />
+    <Post 
+    user_id = "user_id.test"
+    post_id = {123456}
+    text="test"
+    like_count={8888}
+    comment_count = {8888}
+    />
   </div>
 );
 
