a846068e83646999d7cc7df27b6d9816c3d9e576
alfarcats <kuroyuki2039@icloud.com>
2025-02-27 20:03:54 +0900

    fix routing path and query

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        // フロント側で /api/:path* にアクセスしたら、
+        // バックエンド (http://localhost:8080) に転送する設定
+        source: '/:path*',
+        destination: 'http://localhost:8080/:path*',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
